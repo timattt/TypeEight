99acca488314f7db05dca3cc07c79662fbd5b05b
timattt <timattt@gmail.com>
2024-09-19 19:42:11 +0300

    resolving timattt/TypeSix#51
    changed url

--- src/store/constants.js
+++ src/store/constants.js
@@ -9,7 +9,7 @@ export const CLIENT_ID = process.env.REACT_APP_TYPE8_CLIENT_ID
 export const CLIENT_SECRET = process.env.REACT_APP_TYPE8_CLIENT_SECRET
 export const REDIRECT_URI = process.env.REACT_APP_TYPE8_REDIRECT_URI
 
-export const OAUTH2_TOKEN_ENDPOINT = "/public/oauth2/token"
-export const OAUTH2_AUTHORIZATION_ENDPOINT = "/public/oauth2/authorize"
-export const LOGOUT_ENDPOINT = "/public/logout"
-export const INTROSPECTION_ENDPOINT = '/public/oauth2/introspect'
+export const OAUTH2_TOKEN_ENDPOINT = "/api/v1/public/oauth2/token"
+export const OAUTH2_AUTHORIZATION_ENDPOINT = "/api/v1/public/oauth2/authorize"
+export const LOGOUT_ENDPOINT = "/api/v1/public/logout"
+export const INTROSPECTION_ENDPOINT = '/api/v1/public/oauth2/introspect'
